0114d5bb32afde38377b06b4b94bc600dffe54bc
DESKTOP-KRH62SM\pican <picanarachu@gmail.com>
2023-09-24 00:51:58 +0900

    デバッグバーの実装完了。 少し、マイグレーションとシードをやってある

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'over_name' => '晴天',
+            'under_name' => '空',
+            'over_name_kana' => 'はるぞら',
+            'under_name_kana' => 'そら',
+            'mail_address' => 'sorazora@gmail.com',
+            'birth_day' => '19980524',
+            'password' => bcrypt('sorazora'),
+        ]);
 
     }
 }
