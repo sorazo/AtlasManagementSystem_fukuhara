25122e71bd63103a551a13ace485c80130200780
DESKTOP-KRH62SM\pican <picanarachu@gmail.com>
2023-09-26 16:12:51 +0900

    usersテーブルとsubjectsテーブルのリレーションを定義 多　対　多

--- app/Models/Users/Subjects.php
+++ app/Models/Users/Subjects.php
@@ -16,6 +16,7 @@ class Subjects extends Model
     ];
 
     public function users(){
-        return;// リレーションの定義
+        // リレーションの定義
+        return $this->belongsToMany('App\Models\Users\User', 'subject_users', 'subject_id', 'user_id');
     }
 }
--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -67,7 +67,8 @@ class User extends Authenticatable
     }
 
     public function subjects(){
-        return ;// リレーションの定義
+        // リレーションの定義
+        return $this->belongsToMany('App\Models\Users\Subjects', 'subject_users', 'user_id', 'subject_id');
     }
 
     // いいねしているかどうか
