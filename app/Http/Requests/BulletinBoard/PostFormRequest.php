f6ac293ad86dc074e76682db9727e98cd512025f
DESKTOP-KRH62SM\pican <picanarachu@gmail.com>
2023-10-06 16:16:45 +0900

    投稿の編集/削除

--- app/Http/Requests/BulletinBoard/PostFormRequest.php
+++ app/Http/Requests/BulletinBoard/PostFormRequest.php
@@ -24,17 +24,19 @@ class PostFormRequest extends FormRequest
     public function rules()
     {
         return [
-            'post_title' => 'min:4|max:50',
-            'post_body' => 'min:10|max:500',
+            'post_title' => 'required|string|max:100',
+            'post_body' => 'required|string|max:5000',
         ];
     }
 
     public function messages(){
         return [
-            'post_title.min' => 'パスワードは8文字以上入力してください。',
-            'post_title.max' => 'パスワードは30文字以内で入力してください。',
-            'post_body.min' => '内容は10文字以上入力してください。',
-            'post_body.max' => '最大文字数は500文字です。',
+            'post_title.required' => 'タイトルは必須項目です。',
+            'post_title.string' => '文字列型で入力してください。',
+            'post_title.max' => 'タイトルの最大文字数は100文字です。',
+            'post_body.required' => '投稿内容は必須項目です。',
+            'post_body.string' => '文字列型で入力してください。',
+            'post_body.max' => '投稿の最大文字数は5000文字です。',
         ];
     }
 }
